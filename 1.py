d9cd77c438b8665a93daeef4f22fa5a9901a60ca
dsail <dsail@yandex.ru>
2022-10-15 11:06:50 +0300

    commit 2

--- 1.py
+++ 1.py
@@ -1,4 +1,13 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 # Пример:
 # 6782 -> 23
-# 0,56 -> 11
+# 0.56 -> 11
+
+number = input('Введите пожалуйста число ')
+number = number.replace('.', '')
+size = len (number)
+number = number
+result = 0
+for count in range (size):
+    result = result + int (number [count])
+print (result)
